1e8d31bbe3be1d8a43aadf49a542d7fd0cf906ec
AleksDominik <aleksnougbele@gmail.com>
2019-04-03 18:20:34 +0000

    amelioration pour portofolio et express maitrise etap001

--- app.js
+++ app.js
@@ -3,25 +3,39 @@
 ** pour utiliser ce serveur, il faut charger la page /ioclient.html
 */
 var connect = require('connect')
-  , serveStatic = require('serve-static')
-  , app = connect()
+  , serveStatic = require('serve-static'),
+  express=require('express')
+  ,app=express()
+ /* //, app = connect().use(serveStatic('htdocs', {'index':['pagedacceuill.html']})) // middleware de gestion de pages statiques
+.use(function(request,response) { // middleware maison pour gestion 404
+response.writeHead(404, { 'Content-Type': 'text/plain; charset=utf8' });
+response.end('Désolé, le document demandé est introuvable...');
+})*/
   , http = require('http')
   , server = http.createServer(app)
   , io = require('socket.io').listen(server)
   , port = 8080
 ;
+app.use(express.static(__dirname + '/htdocs',{index:"pagedacceuill.html"}));
 
 /*
 ** static resource server middleware
 */
-app.use(serveStatic('htdocs'));
+//app.use(serveStatic('htdocs'));
 
 
 /*
 ** start server
 */
-server.listen(port);
+/*app.all("/app", function (req, res, next) {
+ console.log("requete envoye a une la page principe");
+res.sendFile('index.html', { root: __dirname + "/htdocs" } );
+
+});*/
+app.listen(port);
+
 var user_count = 0;
+
 
 /*
 ** socket connection listener
