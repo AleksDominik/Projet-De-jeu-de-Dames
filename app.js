8c26f8cbcc4b43ecc38edc6b0c3ff8eda1e300ce
AleksDominik <aleksnougbele@gmail.com>
2019-04-11 23:17:53 +0000

    amelioration du front et des fonctions back

--- app.js
+++ app.js
@@ -3,82 +3,190 @@
 ** pour utiliser ce serveur, il faut charger la page /ioclient.html
 */
 var connect = require('connect')
-  , serveStatic = require('serve-static'),
-  express=require('express')
+  , serveStatic = require('serve-static')
+  ,url = require('url')
+  ,express=require('express')
   ,app=express()
- /* //, app = connect().use(serveStatic('htdocs', {'index':['pagedacceuill.html']})) // middleware de gestion de pages statiques
-.use(function(request,response) { // middleware maison pour gestion 404
-response.writeHead(404, { 'Content-Type': 'text/plain; charset=utf8' });
-response.end('Désolé, le document demandé est introuvable...');
-})*/
-  , http = require('http')
-  , server = http.createServer(app)
+  ,fs = require('fs')
+//  , http = require('http')
+  , server =app.listen(8080) //http.createServer(app)
   , io = require('socket.io').listen(server)
   , port = 8080
 ;
+//app.set('port', process.env.PORT || 8080);
+
 app.use(express.static(__dirname + '/htdocs',{index:"pagedacceuill.html"}));
+//;express.static(__dirname + '/htdocs',{index:"pagedacceuill.html"})
+app.get("/index", function(req, res){
+  res.type('text/plain');
+  
+  express.static(__dirname + '/htdocs',{index:"index.html"});
+  //res.redirect("./htdocs/pagedacceuill.html");console.log("requetedeindex");
 
-/*
-** static resource server middleware
-*/
-//app.use(serveStatic('htdocs'));
 
-
-/*
-** start server
-*/
-/*app.all("/app", function (req, res, next) {
- console.log("requete envoye a une la page principe");
-res.sendFile('index.html', { root: __dirname + "/htdocs" } );
-
+});
+/*app.listen(port, function(){
+  console.log( 'Express started on http://localhost:' +    port + '; press Ctrl-C to terminate.' );
 });*/
 
-server.listen(port);
+function ChercheCoup(Pion, Position,joueur){//coordonne du pion analyser Position des pions sur le tableau et indice du joueur
+ tabpos=[]; 
+ //console.log(Pion);
+  if((Pion[0]+2<10 && Pion[1]+2<10)&& (Position[Pion[0]+1][Pion[1]+1]!==3 &&Position[Pion[0]+1][Pion[1]+1]!==joueur && Position[Pion[0]][Pion[1]]==joueur && Position[Pion[0]+2][Pion[1]+2]==3)){//en bas /a droite
+    console.log("en bas /a droite");
+tabpos.push([Pion,[Pion[0]+1,Pion[1]+1],[Pion[0]+2,Pion[1]+2]]);
+
+  }
+  if((Pion[0]+2<10 && Pion[1]-2>0  )  &&(Position[Pion[0]+1][Pion[1]-1]!=3 &&Position[Pion[0]+1][Pion[1]-1]!=joueur&& Position[Pion[0]][Pion[1]]==joueur  && Position[Pion[0]+2][Pion[1]-2]==3)){// en bas a gauche
+tabpos.push([Pion,[Pion[0]+1,Pion[1]-1],[Pion[0]+2,Pion[1]-2]]);
+ console.log("en bas a gauche");
+  }
+  if((Pion[0]-2>0 && Pion[1]+2<10) &&(Position[Pion[0]-1][Pion[1]+1]!=3 &&Position[Pion[0]-1][Pion[1]+1]!=joueur&& Position[Pion[0]][Pion[1]]==joueur  && Position[Pion[0]-2][Pion[1]+2]==3)){// en haut a droite
+tabpos.push([Pion,[Pion[0]-1,Pion[1]+1],[Pion[0]-2,Pion[1]+2]]);
+ console.log("en haut a droite");
+  }
+  if((Pion[0]-2>0 && Pion[1]-2>0)&&(Position[Pion[0]-1][Pion[1]-1]!=3 &&Position[Pion[0]-1][Pion[1]-1]!=joueur && Position[Pion[0]][Pion[1]]==joueur  && Position[Pion[0]-2][Pion[1]-2]==3)){// en haut a gauche
+tabpos.push([Pion,[Pion[0]-1,Pion[1]-1],[Pion[0]-2,Pion[1]-2]]);
+ console.log("en haut a gauche");
+  }
+return tabpos
+
+
+}
+//server.listen(port);
+
+
 
 
 var user_count = 0;
-function VerifieCoup(Position, tabCoup){//prend la position que le serveur connait et le tableau case depart -case arrive est valable
 
+function RecherchePosition(Position,joueur){
+  var Positionfinal=clone2D(Position);
+ listedespositionpossibles=[];
+  for (var i = 0 ; i <= 9; i++) {
+    for (var j = 0 ;j< 9; j++) {
+      temp=ChercheCoup([i,j], Positionfinal,joueur); //temp peut contenir plusieur coup possibles pour une position
+     if(temp.length!=0){
+     listedescoupspossibles.push(temp);
+     }
+
+
+      // console.log(ChercheCoup(Positionfinal[i][j], Positionfinal,1));
+     }     
+    }
+    if (listedescoupspossibles.length!=0) {
+      console.log("coup possible detecte",listedescoupspossibles);
+      for (var i = 0; i<listedescoupspossibles.length; i++) {
+  Positionfinal=clone2D(Position);
+  temp=listedescoupspossibles[i];
+
+  for (var j = 0; j<temp.length; j++) {
+    temp2=clone2D(temp[j]);
+    console.log("la variable temp2",temp2);
+    pion=Positionfinal[temp2[0][0]][temp2[0][1]];
+     Positionfinal[temp2[0][0]][temp2[0][1]]=3;
+     Positionfinal[temp2[1][0]][temp2[1][1]]=3;
+     Positionfinal[temp2[2][0]][temp2[2][1]]=pion;
+    listedespositionpossibles.push(Positionfinal);
+
+  }
+
+  
+
+  }
+   console.log("les positions possibles",listedespositionpossibles);
+    }
+
+ 
+return listedespositionpossibles;
+}
+function clone2D(a) {
+  return a.map(o => [...o]);
+}
+function VerifieCoup(Position, tabCoup, joueur){//prend la position que le serveur connait et le tableau case depart -case arrive est valable
+var tableaudesposibles;
+
+var Positionfinal=clone2D(Position);
+/*if(Position[tabCoup[0][0]][tabCoup[0][1]]==joueur){
+  return {Posit:Position,error: "case errone"};
+}*/
 //prise en compte de la validiter et des reggggggggles.
-if((tabCoup[1][0]%2==0 && tabCoup[1][1]%2==0)|| ((tabCoup[1][0]%2==0 && tabCoup[1][1]%2==0))){   //il faut etre sur les case valide
+console.log("tabCoup",tabCoup);
+if( ( (tabCoup[1][0]%2==0 && tabCoup[1][1]%2==0)|| (tabCoup[1][0]%2==1 && tabCoup[1][1]%2==1) )&&( (tabCoup[0][0]%2==0 && tabCoup[0][1]%2==0)|| ((tabCoup[0][0]%2==1 && tabCoup[0][1]%2==1)))   ){   //il faut etre sur les case valide
 
-
+if(Positionfinal[tabCoup[1][0]][tabCoup[1][1]]==3){ //arrive vide
+//supp case depart;
+listedescoupspossibles=[];
+Posi1=RecherchePosition(Position,joueur);
+while(Posi1.length>0){
+  for (var h = 0; h<Posi1.length; h++) {
+    
+  }
+}
+ /* for (var i = 0 ; i <= 9; i++) {
+    for (var j = 0 ;j< 9; j++) {
+      temp=ChercheCoup([i,j], Positionfinal,joueur);
+     if(temp.length!=0){
+     listedescoupspossibles.push(temp);
+     }
+     // console.log(ChercheCoup(Positionfinal[i][j], Positionfinal,1));
+     }     
+    }*/
+   
+    //console.log("les prises possibles sont:",listedescoupspossibles);
+   // if(listedescoupspossibles.length==0) {
+      pion=Positionfinal[tabCoup[0][0]][tabCoup[0][1]];
+      Positionfinal[tabCoup[0][0]][tabCoup[0][1]]=3;
+      Positionfinal[tabCoup[1][0]][tabCoup[1][1]]=pion;
+     //}
+  
+  }
 
 }
 
 
+return Positionfinal;
 }
+
 
 function CreerUnjeu(){
 
   this.Listedespion=[];
-   this.temps1;
+  this.tour;
+     this.temps1;
     this.temps1;
+    this.ajouer=function(){
+      if (this.tour==1) {
+        this.tour=2;
+      }
+      else this.tour=1;
+    }
   this.debut=function(){
+    this.tour=1;
     for (var i = 0; i < 10; i++) {
       for (var j = 0;j< 10; j++) {
         if ((i%2==0) &&( j%2==0) && (i<4)) {
          this.Listedespion.push([]);
-          console.log("pion du joueur 1op1",this.Listedespion);
+          //console.log("pion du joueur 1op1",this.Listedespion);
           this.Listedespion[i].push(1);
         }
         else if ((i%2==0 )&& (j%2==0)&& (i>5)) {
           
           this.Listedespion[i].push(2);
 
-          console.log("pion du joueur 2");
+          //console.log("pion du joueur 2");
 
         }
         else if ((i%2==1) && (j%2==1) && (i<4)) {
           //this.Listedespion[i][j]=1;
           this.Listedespion[i].push(1);
-          console.log("pion du joueur 1op2");
+         // console.log("pion du joueur 1op2");
             
         }
         else if ((i%2==1) && (j%2==1) && (i>5)) {
           //this.Listedespion[i][j]=2;
           this.Listedespion[i].push(2);
-          console.log("pion du joueur 2");
+        //  console.log("pion du joueur 2");
 
         }
         else{
@@ -103,6 +211,15 @@ io.on('connection', function (socket) {
   console.log('client connected');
   
   
+  var lejeu=CreerUnjeu();
+
+/*fs.writeFile('./Public/Basedesparties.json', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+*/
+
+
   socket.on('add user', function (username) {
     socket.username = username;
     console.log("new user:"+username+" logged.");
@@ -112,15 +229,27 @@ io.on('connection', function (socket) {
   });
   
   socket.on("debutdepartie",function(){
-      lejeu=CreerUnjeu();
+      
        lejeu.debut();
        console.log(lejeu.Listedespion);
     io.emit("positioninitial",{
-      position:lejeu.Listedespion})
+      position:lejeu.Listedespion,tour:lejeu.tour})
   })
 
 socket.on("coupjouer",function(data){
 console.log("coup joue",data);
+
+if(lejeu.Listedespion[data[0][0]][data[0][1]]==lejeu.tour && lejeu.Listedespion[data[1][0]][data[1][1]]==3){
+lejeu.Listedespion=VerifieCoup(lejeu.Listedespion, data,lejeu.tour);
+console.log(lejeu.Listedespion);
+lejeu.ajouer();
+io.emit("NouvellePosition",{Position:lejeu.Listedespion,tour:lejeu.tour});
+}
+else{  
+
+io.emit("NouvellePosition",{Position:lejeu.Listedespion,tour:lejeu.tour});
+}
+
 
 
 
